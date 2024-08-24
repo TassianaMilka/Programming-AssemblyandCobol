250200cfc174964c7a21d06cfc6b22b210a56e8c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-08-24 15:46:24 -0300

    Create Website.cbl

---
+++ Cobol/Website/Website.cbl
@@ -0,0 +1,13 @@
+* Name the program and end program,author,installation url,date write, date compiled and display mensage Website about IDE.
+
+IDENTIFICATION DIVISION.
+PROGRAM-ID. WEBSITE.
+AUTHOR. Tassiana_Milka.
+INSTALLATION. https://launchpad.net/cobcide/+download .
+DATE-WRITTEN.24/08/2024
+DATE-COMPILED.24/08/204
+REMARKS. Website to dowload
+PROCEDURE DIVISION.
+DISPLAY "Website about IDE"
+STOP RUN.
+END PROGRAM WEBSITE.
