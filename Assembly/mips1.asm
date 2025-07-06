f63339f0c4dfefd08359074cfce3ed45ce6bde0c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-07-06 15:01:06 -0300

    1

---
+++ Assembly/mips1.asm
@@ -0,0 +1,11 @@
+.data
+       #Print message 
+    msg:.asciiz "Hello!"
+    
+.text
+     #Instrucion
+     
+     #Print string
+      li $v0, 4 
+      la $a0,msg # about message
+      Syscall #read print
