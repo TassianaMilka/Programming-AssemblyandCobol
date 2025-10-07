1072e711767e6ced75b733350d5ed957d476ba42
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-07 15:58:34 -0300

    Delete Assembly directory

--- Assembly/mips3/mips3.asm
+++
@@ -1,7 +0,0 @@
-.data
-      idade: .vord 78 #type 
-.text
-
-     li $v0,1
-     lw $a0,idade #print about name type idade
-     syscall
--- Assembly/mips4/mips4.asm
+++
@@ -1,5 +0,0 @@
-.text
-      #Print about +
-       li $t0, 23
-       li $t1, 23
-       add $s0, $t0, $t1
