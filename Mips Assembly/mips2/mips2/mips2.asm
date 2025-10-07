d51a600f0c4db14bc0eb6ab0ba682b24ce34dca0
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-07 15:53:21 -0300

    Create mips2.asm

---
+++ Mips Assembly/mips2/mips2/mips2.asm
@@ -0,0 +1,13 @@
+#Print one char T
+
+.data
+       #Print char
+    caractere: .byte 'T'
+    
+.text
+     #Instrucion
+     
+     #Print string
+      li $v0, 4 
+      la $a0,caractere# about message
+      Syscall #read print
