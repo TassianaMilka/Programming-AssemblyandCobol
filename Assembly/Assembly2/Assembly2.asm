2c6a3e089ae5a229a11df4b0915c99a1c6af4339
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-16 17:45:55 -0300

    Create Assembly2.asm for user input handling
    This assembly program prompts the user for their name, reads the input, and exits.

---
+++ Assembly/Assembly2/Assembly2.asm
@@ -0,0 +1,41 @@
+
+segment .data
+  LF        equ 0xA  ; 
+  NULL      equ 0xD  ; 
+  SYS_CALL  equ 0x80 ; Envia informacao ao SO
+  ; EAX
+  SYS_EXIT  equ 0x1  ; 
+  SYS_READ  equ 0x3  ; 
+  SYS_WRITE equ 0x4  ; 
+  ; EBX
+  RET_EXIT  equ 0x0  ; 
+  STD_IN    equ 0x0  ; Entrada padrao
+  STD_OUT   equ 0x1  ; Saida padrao
+
+section .data
+  msg db "Entre com seu nome:", LF, NULL
+  tam equ $- msg
+
+section .bss
+  nome resb 1  
+
+section .text  
+
+global _start
+
+_start:
+  mov EAX, SYS_WRITE
+  mov EBX, STD_OUT
+  mov ECX, msg
+  mov EDX, tam
+  int SYS_CALL
+
+  mov EAX, SYS_READ
+  mov EBX, STD_IN
+  mov ECX, nome
+  mov EDX, 0xA
+  int SYS_CALL
+
+  mov EAX, SYS_EXIT
+  mov EBX, RET_EXIT
+  int SYS_CALL
