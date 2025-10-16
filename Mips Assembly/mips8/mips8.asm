6be497c6fd088297fe8bb5d29e896b39c004bd4b
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-16 14:40:49 -0300

    Add age input and output functionality in MIPS
    This MIPS assembly program prompts the user to enter their age and then displays it back.

---
+++ Mips Assembly/mips8/mips8.asm
@@ -0,0 +1,26 @@
+.data
+         #Print  enter age 
+        saudacao:  .asciiz  "Informe sua idade"
+        saida:  .asciiz   "Sua idade:"
+
+.text
+
+       li $v0,4
+       li $a0, saudacao
+       syscall
+
+
+      li $v0 ,5
+      syscall
+
+      move $t0, $v0
+
+       li $v0,4
+       li $a0, saida
+       syscall
+
+       li $v0,1
+       move $a0, $t0
+       syscall
+
+
