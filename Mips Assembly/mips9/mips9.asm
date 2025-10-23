b2503fdccc1f29b69bbb444c57f9a03cbe539f6d
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-23 11:52:01 -0300

    Implement user input and greeting prompts
    Added prompts for user input and greeting in MIPS assembly.

--- Mips Assembly/mips9/mips9.asm
+++ Mips Assembly/mips9/mips9.asm
@@ -1,1 +1,32 @@
+.data
+
+       enter: .asciiz "Enter name"
+       next: .asciiz  "Olá, "
+       name: .space 25
+
+.text
+
+
+
+       li $v0, 4
+       la $a0,enter
+       syscall
+
+
+       li $v0,8
+       la $a0,name
+       la $a1,25
+       syscall
+
+
+      li $v0,4
+      la $a0, next
+      syscall
+
+
+
+
+
+
+
 
