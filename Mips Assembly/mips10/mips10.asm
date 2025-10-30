be21a5b45d06a7b888c22671f8ae73e4ba7b4bc8
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-10-30 16:18:17 -0300

    Create mips10.asm

---
+++ Mips Assembly/mips10/mips10.asm
@@ -0,0 +1,22 @@
+.data
+
+    msg: .asciiz "Enter one number:"
+    zero:  .float 0.0
+
+.text
+
+    #print number
+    li $v0,4
+    la $a0,msg
+    syscall
+
+
+    li $v0,6
+    sysccall
+
+    lwc1  $f1,zero
+    add.s $f12, $f1, $f0
+
+    li $v0, 2
+    syscall 
+
