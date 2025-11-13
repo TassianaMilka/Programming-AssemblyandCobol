eb3659193f64973b205e4e006adb4575c642718e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-11-13 17:39:47 -0300

    Update mips12.asm

--- Mips Assembly/Basic/mips12/mips12.asm
+++ Mips Assembly/Basic/mips12/mips12.asm
@@ -1,1 +1,34 @@
+.data
+
+        espaco:.byte '  '
+
+.text
+
+     li $v0,5
+     syscall
+
+    move $t0,$v0
+
+    move $t1,$zero
+
+
+   loop:
+
+        bgt $t1, $t2,next
+
+       li $v0,1
+       move $a0,$t1
+       syscall
+
+      li $v0,4
+      la $a0,space
+      syscall
+
+      #Result add
+      addi,$t1,$t1,1
+      j loop
+
+
+  next:
+
 
